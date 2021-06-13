3e150d0b6d83a1c93b3dad23cac764123884752c
Samuferr <samuferr@gmail.com>
2021-06-13 15:40:53 -0300

    Installing Driver mysql, Connection with database and Create Database

---
+++ src/jdbc/FactoryConnection.java
@@ -0,0 +1,19 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FactoryConnection {
+	public static Connection getConnection(){
+		try {
+			final String url = "jdbc:mysql://localhost";
+			final String usuario = "root";
+			final String senha = "12345678";
+			
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/jdbc/TesteConnection.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConnection {
+
+	public static void main(String[] args) throws SQLException {
+		final String url = "jdbc:mysql://localhost";
+		final String usuario = "root";
+		final String senha = "12345678";
+		
+		Connection conexao = DriverManager.getConnection(url, usuario, senha);
+		
+		System.out.println("Conexao efetuada com sucesso!");
+		
+		conexao.close();
+		
+		
+		
+	}
+
+}
